85f4dfba293f464ac20265055e010a70547c0a39
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-09-17 19:58:05 +0530

    Sum of numbers until n/ Sum of distinct n numbers using iteration

--- Assignment-1/A1Q1.java
+++ Assignment-1/A1Q1.java
@@ -1,3 +1,6 @@
+// Qs. 1 :- Sum of n numbers (n-> User Input , integers -> user input)
+// Sum Until n  
+// Sum of distinct n numbers
 import java.util.*;
 public class A1Q1{
     public static void main(String[] args) {
@@ -5,14 +8,12 @@ public class A1Q1{
         System.out.println("Enter a number : ");
         int n = sc.nextInt();
         int sum1 = 0 , sum2 = 0 ;
-        for(int i = 1 ; i<= n ; i++){
-            sum1+=i;
-        }
-        System.out.println("The sum of numbers until n is : "+ sum1);
-        for(int i = 0 ; i< n ; i++){
+        for(int i = 1 ; i<= n ;i++){
+            sum1+=i; // sum of numbers until n 
             int j = sc.nextInt();
-            sum2+=j;
+            sum2+=j; // sum of n distinct numbers
         }
+        System.out.println("The sum of numbers until "+ n + " is : "+ sum1);
         System.out.println("The sum of "+ n + " distinct numbers is : "+ sum2);
         sc.close();
     }
