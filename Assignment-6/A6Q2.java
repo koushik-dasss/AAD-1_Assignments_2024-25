1782e24edbd930b687501dc273f2aff91de72d03
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-01-07 19:38:56 +0530

    A6Q2 added , linear search using recursion

---
+++ Assignment-6/A6Q2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class A6Q2 {
+    public static int LinearSearchRecursion(int[] arr , int ele, int index){
+       if(index > arr.length){
+        return -1;
+       }else if(arr[index]==ele){
+        return index;
+       }
+        return LinearSearchRecursion(arr, ele, index+1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements of the array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array : ");
+        for(int i = 0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The elements of the array are :");
+        for(int i = 0 ; i<n ;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Enter the element to be found : ");
+        int ele = sc.nextInt();
+        int index = 0 ;
+        int result = LinearSearchRecursion(arr , ele , index);
+        if(result!=-1){
+            System.out.println("The element is"+ ele+ " found at index : "+result);
+        }else{
+            System.out.println("The element is not found in the array");
+        }
+        sc.close();
+    }
+}
